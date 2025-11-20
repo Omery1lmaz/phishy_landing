9bf350b2ba211822d465df9056562211d456a939
Omer Farul Yilmaz <omery020040@gmail.com>
2025-11-20 19:09:08 +0300

    update

--- app/api/blog/slug/route.ts
+++ app/api/blog/slug/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getCorrespondingBlogSlug } from '@/lib/blog';
 
+export const runtime = 'edge';
+
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const currentSlug = searchParams.get('slug');
